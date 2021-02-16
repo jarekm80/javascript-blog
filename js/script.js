fbbdb5fde8d913ad4883af10ec79d50a816dbb77
Jarek Matwijewicz <alterro@gmail.com>
2021-02-16 18:31:07 +0100

    finish titleClickHandler

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@
 */
 
 const titleClickHandler = function(event){
-    const clickedElement = this;
+  event.preventDefault();  
+  const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
@@ -16,8 +17,7 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
     }
   
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
+    /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
@@ -26,15 +26,17 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
   
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');  
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
+    for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
